f14210dd5c7b463008c17b09f321d9a616d188bc
davimoreira06 <davimoreira291273@gmail.com>
2023-03-12 19:09:34 -0300

    interacao-usuario

--- scripts.js
+++ scripts.js
@@ -1,5 +1,38 @@
-function receiveUserNumber() {
-    let userNumber = document.getElementById("card-number").value;
-    console.log(userNumber)
+const pNumber = document.getElementById("user-number");
+const inpNumber = document.getElementById("card-number");
+
+const pName = document.getElementById("user-name");
+const inpName = document.getElementById("card-name");
+
+const pMonth = document.getElementById("user-exp-date-m");
+const selMonth = document.getElementById("exp-date-m");
+
+const pYear = document.getElementById("user-exp-date-y");
+const selYear = document.getElementById("exp-date-y");
+
+const pCvc = document.getElementById("user-cvc");
+const inpCvc = document.getElementById("cvc");
+
+
+function receiveName() {
+    pName.innerHTML = inpName.value;
+} 
+
+function receiveNumber() {
+    pNumber.innerHTML = format(inpNumber.value);
+}
+function format(s) {
+    return s.toString().replace(/\d{4}(?=.)/g, "$& ");
 }
 
+function receiveMonth() {
+    pMonth.innerHTML = selMonth.value;
+}
+
+function receiveYear() {
+    pYear.innerHTML = selYear.value;
+}
+
+function receiveCvc() {
+    pCvc.innerHTML = inpCvc.value;
+}
